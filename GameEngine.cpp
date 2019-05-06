39e48794cfe15e0f3f170035c52447f33ae0f49e
Matthew Samuel <s3717393@student.rmit.edu.au>
2019-05-06 12:54:39 +1000

    Small changes to saveGame method

--- GameEngine.cpp
+++ GameEngine.cpp
@@ -11,13 +11,23 @@ void GameEngine::saveGame(std::string filename) {
   outFile.open(filename);
 
   // Writing out player data
-  outFile << /*player1 <<*/ std::endl;
-
+  outFile << /*player1->getName() << */ std::endl;
+  outFile << /*player1->getPoints() << */ std::endl;
   // Would probabbly want a toString method for LinkedList so that it prints out
   // the list in the format needed. Would be way more efficent than doing it in
-  // in the GameEngine ('for' loop that would call some sort of getAt(i) method).
+  // the GameEngine ('for' loop that would call some sort of getAt(i) method).
+  outFile << /*player1->getHand.toString() << */ std::endl;
+
+  outFile << /*player2->getName() << */ std::endl;
+  outFile << /*player2->getPoints() << */ std::endl;
+  outFile << /*player2->getHand.toString() << */ std::endl;
 
   // Writing out board to file
+  outFile << "   0  1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 17 18 19 20"
+  << " 22 23 24 25" << std::endl;
+  outFile << "-----------------------------------------------------------------"
+  << "----------------" << std::endl;
+
   for (int i = 0; i < BOARD_SIZE; i++) {
     // ASCII character table states that capital letters begin at 65, so with
     // each iteration the character goes up (From 'A' to 'B' to 'C'...).
@@ -38,7 +48,7 @@ void GameEngine::saveGame(std::string filename) {
   outFile << /*tileBag->toString(); << */ std::endl;
 
   // Writing out current player to file
-  out << /*getCurrentPlayer() << */ std::endl;
+  outFile << /*currentPlayer->getName() << */ std::endl;
 
   outFile.close();
 }
